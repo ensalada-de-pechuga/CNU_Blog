471432b4e0a4ecb45e3e407cc1cb27893e4519e7
ensalada-de-pechuga <villop020403@o.cnu.ac.kr>
2024-06-02 17:33:34 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
 import Home from './pages/Home';
 import Layout from './pages/Layout';
@@ -8,19 +9,23 @@ import Write from './pages/Write';
 import Header from './components/Header';
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <Router>
-      <Routes>
-        <Route element={<Header />}>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/resume" element={<Resume />} />
+    <QueryClientProvider client={queryClient}>
+      <Router basename={'/CNU_Blog/'}>
+        <Routes>
+          <Route element={<Header />}>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/resume" element={<Resume />} />
+            </Route>
+            <Route path="/posts/:postId" element={<Post />} />
           </Route>
-          <Route path="/posts/:postId" element={<Post />} />
-        </Route>
-        {/*todo (5-1) Write 추가*/}
-      </Routes>
-    </Router>
+          <Route path="/Write" element={<Write />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
