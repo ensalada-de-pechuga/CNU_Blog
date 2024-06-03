88d1e84844958a35804558717178246f99b4060f
ensalada-de-pechuga <villop020403@o.cnu.ac.kr>
2024-06-03 14:29:54 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
 import Home from './pages/Home';
